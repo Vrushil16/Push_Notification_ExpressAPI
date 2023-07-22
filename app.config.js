2e3436138417dc9abbd0e67d6bc09b1531948748
Vrushil Parekh <121599252+Vrushil16@users.noreply.github.com>
2023-07-22 17:56:49 +0530

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,8 @@
+const ONE_SIGNAL_CONFIG ={
+    APP_ID:"027ebfe1-96cc-49c2-ade4-f8d0eb23c242",
+    API_KEY:"NTU4Njk5NDYtMDJiOS00MzVhLWI3ZjYtN2UyMWZjMDRiMTFj",
+};
+
+module.exports = {
+    ONE_SIGNAL_CONFIG,
+}
